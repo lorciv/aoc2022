7a81e5820e43de2556177a6b047e991d9059a796
Lorenzo Civolani <lorenzo.civolani@unibo.it>
2022-12-01 14:54:06 +0100

    day 1 part 1

---
+++ day01/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	max, sum := 0, 0
+	scan := bufio.NewScanner(os.Stdin)
+	for scan.Scan() {
+		line := scan.Text()
+		if line == "" {
+			if sum > max {
+				max = sum
+			}
+			sum = 0
+			continue
+		}
+
+		n, _ := strconv.Atoi(line)
+		sum += n
+	}
+	if err := scan.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(max)
+}
